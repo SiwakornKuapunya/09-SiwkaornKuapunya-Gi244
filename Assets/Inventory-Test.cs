255beb9ce0ed75ccffe485a2037c6fce4d5803f2
SiwakornKuapunya <siwakorn.kuap@bumail.net>
2024-01-11 10:54:04 +0700

    Dislay inventory

--- Assets/Inventory-Test.cs
+++ Assets/Inventory-Test.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //inventory array
+    private string[] inventoryArray = new[] {"Sword","Shield","Potion"};
+    
+    //move elements in the array to list
+    private List<string> inventoryList;
+    
+     void Start()
+     {
+         inventoryList = new List<string>(inventoryArray);
+         DisplayInventoryList();
+     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inventoryList.Add("Helmet");
+            DisplayInventoryList();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            inventoryList.Remove("Sword");
+            DisplayInventoryList();
+        }
     }
+
+    void DisplayInventoryList()
+    {
+        int i = 0;  
+        Debug.Log("Inventory:");
+        foreach (var item in inventoryList)
+        {
+            Debug.Log($"{++i}. {item}");
+        }
+    }
+    
 }
